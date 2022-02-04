358b459abef76d22ed938a44b9d4e30513a206e7
RodionIK <bcons@yandex.ru>
2022-02-04 10:15:38 +0300

    Исправлено после ревью: доработать метод FTD с одним параметром и улучшить наименование типов и переменных

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -80,10 +80,10 @@ public:
                            });
     }
 
-    template <typename FilterFunc>
-    vector<Document> FindTopDocuments(const string& raw_query, FilterFunc filterfunc) const {
+    template <typename Filter>
+    vector<Document> FindTopDocuments(const string& raw_query, Filter filter_function) const {
         const Query query = ParseQuery(raw_query);
-        auto matched_documents = FindAllDocuments(query, filterfunc);
+        auto matched_documents = FindAllDocuments(query, filter_function);
 
         sort(matched_documents.begin(), matched_documents.end(),
              [](const Document& lhs, const Document& rhs) {
@@ -99,12 +99,12 @@ public:
         return matched_documents;
     }
 
-    vector<Document> FindTopDocuments(const string& raw_query) const {
-        return FindTopDocuments(raw_query, [](int document_id, DocumentStatus status, int rating){ return status == DocumentStatus::ACTUAL;});
-    }
-
     vector<Document> FindTopDocuments(const string& raw_query, DocumentStatus status) const {
         return FindTopDocuments(raw_query, [status](int document_id, DocumentStatus lstatus, int rating){ return lstatus == status;});
+    }
+
+    vector<Document> FindTopDocuments(const string& raw_query) const {
+        return FindTopDocuments(raw_query, DocumentStatus::ACTUAL);
     }
 
     int GetDocumentCount() const {
@@ -214,8 +214,9 @@ private:
         return log(GetDocumentCount() * 1.0 / word_to_document_freqs_.at(word).size());
     }
 
-    template <typename FilterFunc>
-    vector<Document> FindAllDocuments(const Query& query, FilterFunc filterfunc) const {
+    template <typename Filter
+            >
+    vector<Document> FindAllDocuments(const Query& query, Filter filter_function) const {
         map<int, double> document_to_relevance;
         for (const string& word : query.plus_words) {
             if (word_to_document_freqs_.count(word) == 0) {
@@ -223,7 +224,8 @@ private:
             }
             const double inverse_document_freq = ComputeWordInverseDocumentFreq(word);
             for (const auto [document_id, term_freq] : word_to_document_freqs_.at(word)) {
-                if (filterfunc(document_id, documents_.at(document_id).status, documents_.at(document_id).rating)) {
+                const auto& document_data = documents_.at(document_id);
+                if (filter_function(document_id, document_data.status, document_data.rating)) {
                     document_to_relevance[document_id] += term_freq * inverse_document_freq;
                 }
             }
