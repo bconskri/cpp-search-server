a5b42bdbee83689e482651892fe256639f78e8ba
RodionIK <bcons@yandex.ru>
2022-05-20 08:39:39 +0300

    Урок 9: Параллелим методы системе 3/3 Корректировка типов в SplitIntoWordsView

--- search-server/string_processing.cpp
+++ search-server/string_processing.cpp
@@ -28,10 +28,10 @@ std::vector<std::string> SplitIntoWords(const std::string_view text) {
 std::vector<std::string_view> SplitIntoWordsView(std::string_view str) {
     std::vector<std::string_view> result;
     str.remove_prefix(std::min(str.find_first_not_of(' '), str.size()));
-    const std::string::size_type pos_end = str.npos;
+    const std::string_view::size_type pos_end = str.npos;
 
     while (str.size() > 0) {
-        std::string::size_type space = str.find(' ');
+        std::string_view::size_type space = str.find(' ');
         result.push_back(space == pos_end ? str.substr(0, str.size()) : str.substr(0, space));
         str.remove_prefix(std::min(str.find_first_not_of(' ', space), str.size()));
     }
